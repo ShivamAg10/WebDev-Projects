0f0a106d5bda3876e58eb71b9f08a19aa9327ef5
Shivam1848 <kashokagrawal1848@gmail.com>
2024-02-02 06:29:49 +0530

    Day 2

--- agoda/accounts/views.py
+++ agoda/accounts/views.py
@@ -1,8 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 # Create your views here.
 def createAccount(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        first_name = request.POST.get("fname")
+        last_name = request.POST.get("lname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        cpassword = request.POST.get("cpassword")
+
+        if password == cpassword:
+            new_user = User.objects.create(
+                username = username,
+                first_name = first_name,
+                last_name = last_name,
+                email = email
+            )
+
+            new_user.set_password(password)
+            new_user.save()
+
+            return redirect("/accounts/signin")
+
+        else:
+            messages.error(request, "Maybe Password and Confirm Password is not same")
+
     return render(request, "accounts/createAccount.html")
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = auth.authenticate(
+            username = username,
+            password = password
+        )
+
+        if user is None:
+            messages.error(request, "Maybe you have not created account....")
+        else:
+            auth.login(request, user)
+            return redirect("/")
+
     return render(request, "accounts/signin.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect("signin")
--- agoda/home/views.py
+++ agoda/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    user = request.user
+    parameters = {
+        user : user,
+    }
+    return render(request, "index.html", parameters)
